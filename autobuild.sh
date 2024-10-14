a5101585b73315c42bb1a27fd2d96c0c1a0cb094
yzfzzz <18320300436@163.com>
2024-10-14 11:00:01 +0800

    🐳 chore: 编译自动化

---
+++ autobuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -d "build" ]; then
+    echo "Build directory already exists."
+    rm -r build
+else
+    echo "Created build directory."
+fi
+
+mkdir build
+cd build
+cmake ..
+make -j2
+cd ..
+rm -r build
