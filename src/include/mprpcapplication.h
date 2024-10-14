1308d856370bb5eea3b041a1308120f64f808001
yzfzzz <18320300436@163.com>
2024-10-14 10:59:24 +0800

    ✨ feat: 新增加载config配置文件功能

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,16 +1,17 @@
 #pragma once
-
+#include "mprpcconfig.h"
 // mprpcøÚº‹≥ı ºªØ¿‡
 class MprpcApplication
 {
     public:
-        static void Init(int argc, char** argvc);
+        static void Init(int argc, char** argv);
         static MprpcApplication& GetInstance()
         {
             static MprpcApplication app;
             return app;
         }
     private:
+        static MprpcConfig m_config;
         MprpcApplication();
         MprpcApplication(const MprpcApplication&) = delete;
         MprpcApplication(MprpcApplication&&) = delete;
