8fe0b73e6cc82318450dc9a952f7fefafc3b3912
yzfzzz <18320300436@163.com>
2024-10-15 16:55:56 +0800

    ✨ feat(RpcProvider): 新增Rpc网络服务、Rpc发布服务方法

--- src/rpcprovider.cpp
+++ src/rpcprovider.cpp
@@ -1,6 +1,59 @@
 #include "rpcprovider.h"
+#include "mprpcapplication.h"
 
-void RpcProvider::NotifyService(google::protobuf::Service* service){
+/*
+service_name => service描述
+                        ==> service* 记录服务对象
+                        method_name => method方法对象
+*/
+void RpcProvider::NotifyService(google::protobuf::Service* service) {
+    ServiceInfo service_info;
+    // 获取服务对象的描述信息
+    const google::protobuf::ServiceDescriptor* pserviceDesc = service->GetDescriptor();
+    // 获取服务的名字
+    std::string service_name = pserviceDesc->name();
+    // 获取服务对象service的方法数量
+    int methodCnt = pserviceDesc->method_count();
 
+    std::cout << "service_name: " << service_name << std::endl;
+
+    for (int i = 0; i < methodCnt; i++) {
+        // 获取了服务对象指定下标的服务方法的描述(抽象描述)
+        const google::protobuf::MethodDescriptor* pmethodDesc = pserviceDesc->method(i);
+        std::string method_name = pmethodDesc->name();
+        service_info.m_methodMap.insert({method_name, pmethodDesc});
+        std::cout << "method_name: " << method_name << std::endl;
+    }
+
+    service_info.m_service = service;
+    m_serviceMap.insert({service_name, service_info});
 }
 
+void RpcProvider::Run() {
+    std::string ip = MprpcApplication::GetInstance().GetConfig().Load("rpcserverip");
+    uint16_t port = atoi(MprpcApplication::GetInstance().GetConfig().Load("rpcserverip").c_str());
+    muduo::net::InetAddress address(ip, port);
+
+    // ------------------------muduo------------------------------------
+    // 启动Tcp Server
+    // 创建TcpServer对象
+    muduo::net::TcpServer server(&m_eventLoop, address, "RpcProvider");
+    // 绑定连接回调和消息回调方法, 分离了网络代码和业务代码
+    // 将RpcProvider::OnConnection(conn)中的参数设置为this
+    server.setConnectionCallback(
+        std::bind(&RpcProvider::OnConnection, this, std::placeholders::_1));
+
+    server.setMessageCallback(std::bind(&RpcProvider::OnMessage, this, std::placeholders::_1,
+                                        std::placeholders::_2, std::placeholders::_3));
+    // 设置muduo库的线程数量
+    server.setThreadNum(4);
+
+    // 启动网络服务
+    server.start();
+    m_eventLoop.loop();
+}
+
+void RpcProvider::OnConnection(const muduo::net::TcpConnectionPtr& conn) {}
+
+void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr&, muduo::net::Buffer*,
+                            muduo::Timestamp) {}
