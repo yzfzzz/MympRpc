0877b650007b303da9f2d43cb098fe23010cc548
yzfzzz <18320300436@163.com>
2024-10-18 11:27:41 +0800

    ✨ feat: 新增客户端远程调用rpc框架

---
+++ example/caller/calluserservice.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "mprpcapplication.h"
+#include "mprpcchannel.h"
+#include "user.pb.h"
+int main(int argc, char** argv) {
+    // 整个程序启动后, 想使用mprpc框架来享受rpc的服务调用, 一定要想调用框架的初始化函数
+    MprpcApplication::Init(argc, argv);
+    // 客户端使用的是UserServiceRpc_Stub
+    // 演示调用远程发布的rpc方法
+    fixbug::UserServiceRpc_Stub stub(new MprpcChannel());  // RpcChannel可以理解为中介
+    fixbug::LoginRequest request;
+    request.set_name("zhang san");
+    request.set_pwd("123456");
+    // rpc方法的响应
+    fixbug::LoginResponse response;
+    // 客户端调用Login, 实际上都是调用该函数RpcChannel->RpcChannel::CallMethod
+    stub.Login(nullptr, &request, &response, nullptr);
+
+    // 一次rpc调用完成, 读调用结果
+    if (response.result().errcode() == 0) {
+        // 调用正确
+        std::cout << "rpc login response:" << response.success() << std::endl;
+    } else {
+        // 调用失败
+        std::cout << "rpc login response error:" << response.result().errmsg() << std::endl;
+    }
+
+
+    
+    return 0;
+}
